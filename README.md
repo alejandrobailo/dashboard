d4aebc36d54ef04671a2ba7ed13f0a5c8e7accc6
Alejandro <alejandrobailo94@gmail.com>
2020-03-07 18:19:34 +0100

    Estructura

--- README.md
+++ README.md
@@ -25,3 +25,10 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+## Libraries
+
+npm i @angular/material
+npm i highcharts-angular
+npm i highcharts
+npm i @angular/flex-layout @angular/cdk --save
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { MatDividerModule, MatToolbarModule, MatIconModule, MatButtonModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout'
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent
+  ],
+  imports: [
+    CommonModule,
+    MatDividerModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    FlexLayoutModule
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent
+  ]
+})
+export class SharedModule { }
